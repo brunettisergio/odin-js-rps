ab9a81323143b713ee8685418214ac7b868d7bf2
Sergio <brunettisergio.dev@gmail.com>
2022-06-06 19:15:21 -0400

    Add divs to separate sections in index.html, as well as text content and buttons.
    Replace console functionality with DOM changes in script.js to make website interactive.

--- script.js
+++ script.js
@@ -1,50 +1,60 @@
 let playerWins = 0,
-    computerWins = 0;
-    
-function game() {
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound());
-        console.log("Player " + playerWins + " - Computer " + computerWins);
+    computerWins = 0,
+    playerSelection = 0,
+    computerSelection = 0;
+
+function playRound(playerSelection) {
+    if (playerWins < 5 && computerWins < 5) {
+        computerSelection = computerPlay();
+        if (playerSelection == computerSelection) {
+            result.textContent = "It's a Tie!";
+        } else if (playerSelection == 1 && computerSelection == 2) {
+            computerWins++;
+            result.textContent = "You lose! Paper beats rock!";
+        } else if (playerSelection == 1 && computerSelection == 3) {
+            playerWins++;
+            result.textContent = "You won! Rock beats scissors";
+        } else if (playerSelection == 2 && computerSelection == 1) {
+            playerWins++;
+            result.textContent = "You won! Paper beats Rock.";
+        } else if (playerSelection == 2 && computerSelection == 3) {
+            computerWins++;
+            result.textContent = "You lose! Scissors beats Paper.";
+        } else if (playerSelection == 3 && computerSelection == 1) {
+            computerWins++;
+            result.textContent = "You lose! Rock beats Scissors.";
+        }  else {
+            playerWins++;
+            result.textContent = "You won! Scissors beats Paper.";
+        }
+    } else if (playerWins == 5) {
+        result.textContent = "Player wins!";
+    } else { 
+        result.textContent = "Computer wins!";
     }
-    if (playerWins == computerWins) console.log("It's a tie!");
-    else if (playerWins > computerWins) console.log("Player wins!");
-    else console.log("Computer wins!")
-}
-
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerPlay();
-    computerSelection = computerPlay();
-    if (playerSelection == computerSelection) return "It's a Tie!";
-    else if (playerSelection == 1 && computerSelection == 2) {
-        computerWins++;
-        return "You lose! Paper beats rock!";
-    } else if (playerSelection == 1 && computerSelection == 3) {
-        playerWins++;
-        return "You won! Rock beats scissors";
-    } else if (playerSelection == 2 && computerSelection == 1) {
-        playerWins++;
-        return "You won! Paper beats Rock.";
-    } else if (playerSelection == 2 && computerSelection == 3) {
-        computerWins++;
-        return "You lose! Scissors beats Paper.";
-    } else if (playerSelection == 3 && computerSelection == 1) {
-        computerWins++;
-        return "You lose! Rock beats Scissors.";
-    }  else {
-        playerWins++;
-        return "You won! Scissors beats Paper.";
-    }
-}
-
-
-function playerPlay() {
-    let selection = prompt("What's your selection?");
-    if (selection.toUpperCase() == "ROCK") return 1;
-    else if (selection.toUpperCase() == "PAPERS") return 2;
-    else if (selection.toUpperCase() == "SCISSORS") return 3;
+    scoreboard.textContent =
+        "Player " + playerWins + " - " + computerWins + " Computer";
 }
 
 function computerPlay() {
     return Math.floor(Math.random() * 3);
 }
 
+const rock = document.querySelector("#rock");
+rock.addEventListener('click', function () {
+    playRound(1)
+});
+
+const paper = document.querySelector("#paper");
+paper.addEventListener('click', function () {
+    playRound(2)
+});
+
+const scissors = document.querySelector("#scissors");
+scissors.addEventListener('click', function () {
+    playRound(3)
+});
+
+const scoreboard = document.querySelector("#scoreboard");
+
+const result = document.querySelector("#result");
