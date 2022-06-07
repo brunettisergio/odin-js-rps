03b9f0e65f143d3f8db2404e4b505aca0d19d3c2
Sergio <brunettisergio.dev@gmail.com>
2022-06-07 16:25:16 -0400

    Refactor game functionality and fix bug were wins were not decided correctly and playerSelection and computerSelection were behaving incorrectly in script.js
    Tweak style for images and buttons in style.css

--- script.js
+++ script.js
@@ -1,81 +1,97 @@
+// Game functionality
 let playerWins = 0,
     computerWins = 0,
-    playerSelection = 0,
-    computerSelection = 0;
+    computerSelection = "",
+    player = "",
+    computer = "";
 
-function playRound(playerSelection) {
+const ROCK = "Rock",
+    PAPER = "Paper",
+    SCISSORS = "Scissors";
+
+function playRound(playerSelection, computerSelection) {
     if (playerWins < 5 && computerWins < 5) {
-        computerSelection = computerPlay();
-        if (playerSelection == computerSelection) {
+        if (playerSelection === computerSelection) {
             result.textContent = "It's a Tie!";
-        } else if (playerSelection == 1 && computerSelection == 2) {
-            computerWins++;
-            result.textContent = "You lose! Paper beats rock!";
-        } else if (playerSelection == 1 && computerSelection == 3) {
+        } else if (
+            (playerSelection === ROCK && computerSelection === SCISSORS) || 
+            (playerSelection === SCISSORS && computerSelection === PAPER) ||
+            (playerSelection === PAPER && computerSelection === ROCK)
+        ) {
             playerWins++;
-            result.textContent = "You won! Rock beats scissors";
-        } else if (playerSelection == 2 && computerSelection == 1) {
-            playerWins++;
-            result.textContent = "You won! Paper beats Rock.";
-        } else if (playerSelection == 2 && computerSelection == 3) {
+            playerScore.textContent = playerWins;
+            result.textContent = 
+                "Player wins the round! " + playerSelection + " beats " + 
+                computerSelection + ".";
+        } else if (
+            (playerSelection === ROCK && computerSelection === PAPER) || 
+            (playerSelection === SCISSORS && computerSelection === ROCK) ||
+            (playerSelection === PAPER && computerSelection === SCISSORS)
+        ) {
             computerWins++;
-            result.textContent = "You lose! Scissors beats Paper.";
-        } else if (playerSelection == 3 && computerSelection == 1) {
-            computerWins++;
-            result.textContent = "You lose! Rock beats Scissors.";
-        }  else {
-            playerWins++;
-            result.textContent = "You won! Scissors beats Paper.";
+            computerScore.textContent = computerWins;
+            result.textContent = 
+                "Computer wins the round! " + computerSelection + " beats " + 
+                playerSelection + ".";
         }
     } else if (playerWins == 5) {
         result.textContent = "Player wins!";
     } else { 
         result.textContent = "Computer wins!";
     }
-    playerScore.textContent = playerWins;
-    computerScore.textContent = computerWins;
 }
 
 function computerPlay() {
-    return Math.floor(Math.random() * 3);
+    let x = Math.floor(Math.random() * 3);
+    if (x == 0) return ROCK;
+    else if (x == 1) return PAPER;
+    else if (x == 2) return SCISSORS;
 }
 
-const rock = document.querySelector("#rock");
-rock.addEventListener('click', function () {
-    playRound(1)
-});
 
-const paper = document.querySelector("#paper");
-paper.addEventListener('click', function () {
-    playRound(2)
-});
-
-const scissors = document.querySelector("#scissors");
-scissors.addEventListener('click', function () {
-    playRound(3)
-});
-
+// HTML Elements.
+const rockButton = document.querySelector("#rock-button");
+const paperButton = document.querySelector("#paper-button");
+const scissorsButton = document.querySelector("#scissors-button");
 const playerScore = document.querySelector(".score.player");
 const computerScore = document.querySelector(".score.computer");
-
 const result = document.querySelector("#result");
-
 const github = document.querySelector("#github");
+const linkedin = document.querySelector("#linkedin");
+const instagram = document.querySelector("#instagram");
+const twitter = document.querySelector("#twitter");
+
+// "Onclick" functions for buttons and social links.
+rockButton.addEventListener('click', function () {
+    player = ROCK;
+    computer = computerPlay();
+    playRound(player, computer);
+});
+
+paperButton.addEventListener('click', function () {
+    player = PAPER;
+    computer = computerPlay();
+    playRound(player, computer);
+});
+
+scissorsButton.addEventListener('click', function () {
+    player = SCISSORS;
+    computer = computerPlay();
+    playRound(player, computer);
+});
+
 github.addEventListener('click', function () {
     window.open("https://github.com/brunettisergio", "_blank")
 });
 
-const linkedin = document.querySelector("#linkedin");
 linkedin.addEventListener('click', function () {
     window.open("https://www.linkedin.com/in/brunettisergio/", "_blank")
 });
 
-const instagram = document.querySelector("#instagram");
 instagram.addEventListener('click', function () {
     window.open("https://www.instagram.com/brunettisergio/", "_blank")
 });
 
-const twitter = document.querySelector("#twitter");
 twitter.addEventListener('click', function () {
     window.open("https://twitter.com/brunettisergio", "_blank")
 });
